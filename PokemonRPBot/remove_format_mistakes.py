16482cb9f239777b7d8f697f339af3eb818994e1
Bahamut <leander.volprich@web.de>
2025-04-26 07:27:02 +0200

    Update

--- PokemonRPBot/remove_format_mistakes.py
+++ PokemonRPBot/remove_format_mistakes.py
@@ -3,7 +3,8 @@ import json
 
 # Define the mapping from the incorrect move strings to the correct ones.
 move_mapping = {
-    "Smoke Screen": "Smokescreen",
+    "Faint Attack": "Feint Attack",
+    "Sand-Attack": "Sand Attack",
 }
 
 def remove_duplicates_preserve_order(seq):
